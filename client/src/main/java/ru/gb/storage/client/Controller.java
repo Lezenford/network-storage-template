0e00d7bab7f6834e517804c689a1e6fa454524ea
Rockerok <Rockerok@mail.ru>
2022-05-17 22:24:39 +0300

    v.002 with Full JavaFX Interface

--- client/src/main/java/ru/gb/storage/client/Controller.java
+++ client/src/main/java/ru/gb/storage/client/Controller.java
@@ -1,6 +1,16 @@
 package ru.gb.storage.client;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Controller {
     private final int PORT = 9000;
@@ -8,5 +18,45 @@ public class Controller {
 
     private String nick;
     public void onSendButtonClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    VBox leftPanel, rightPanel;
+
+    public void btnExitAction(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+
+    public void btnCopyAction(ActionEvent actionEvent) {
+        PanelController leftPanContr = (PanelController) leftPanel.getProperties().get("contr");
+        PanelController rightPanContr = (PanelController) rightPanel.getProperties().get("contr");
+        if (leftPanContr.getSelectedFileName() == null && rightPanContr.getSelectedFileName() == null ){
+            Alert alert= new Alert(Alert.AlertType.ERROR, "File not select", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+        PanelController scrPanContr = null, dstPanContr = null;
+        if (leftPanContr.getSelectedFileName() != null){
+            scrPanContr = leftPanContr;
+            dstPanContr = rightPanContr;
+        }
+        if (rightPanContr.getSelectedFileName() != null){
+            scrPanContr = rightPanContr;
+            dstPanContr = leftPanContr;
+        }
+
+        Path srcPath = Paths.get(scrPanContr.getCurrentPath(),scrPanContr.getSelectedFileName());
+        Path dstPath = Paths.get(dstPanContr.getCurrentPath()).resolve(srcPath.getFileName().toString());
+
+        try {
+            Files.copy(srcPath,dstPath);
+            dstPanContr.updateList(Paths.get(dstPanContr.getCurrentPath()));
+        } catch (IOException e) {
+            Alert alert= new Alert(Alert.AlertType.ERROR, "File is NOT Copy", ButtonType.OK);
+            alert.showAndWait();
+        }
+
+        //Удаление и перемещение https://youtu.be/LILeZhSHf1k?t=5450
+
     }
 }
---
+++ client/src/main/java/ru/gb/storage/client/PanelController.java
@@ -0,0 +1,128 @@
+package ru.gb.storage.client;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+public class PanelController implements Initializable {
+
+        @FXML
+        TableView<FileInfo> filesTable;
+
+        @FXML
+        ComboBox <String> diskBox;
+
+        @FXML
+        TextField pathField;
+
+        @Override
+        public void initialize (URL location, ResourceBundle resources){
+            TableColumn<FileInfo,String> fileTypeColumn = new TableColumn<>();
+            fileTypeColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getType().getName()));
+            fileTypeColumn.setPrefWidth(20);
+
+            TableColumn<FileInfo,String> fileNameColumn = new TableColumn<>(" Name ");
+            fileNameColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getFileName()));
+            fileNameColumn.setPrefWidth(280);
+
+            TableColumn<FileInfo, Long> fileSizeColumn = new TableColumn<>(" Size ");
+            fileSizeColumn.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue().getSize()));
+            fileSizeColumn.setCellFactory(column ->{
+                return new TableCell<FileInfo, Long>() {
+                    @Override
+                    protected void updateItem(Long item, boolean empty){
+                        super.updateItem(item, empty);
+                        if(item == null || empty){
+                            setText (null);
+                            setStyle("");
+                        } else {
+                            String text = String.format("%,d bytes",item);
+                            if (item == -1L){
+                                text = "[DIR]";
+                            }
+                            setText(text);
+                        }
+                    }
+                };
+
+            });
+            fileSizeColumn.setPrefWidth(80);
+
+            DateTimeFormatter datf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            TableColumn<FileInfo,String> fileDateColumn = new TableColumn<>(" Date mod. ");
+            fileDateColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getLastModified().format(datf)));
+            fileDateColumn.setPrefWidth(120);
+
+            filesTable.getColumns().addAll(fileTypeColumn,fileNameColumn,fileSizeColumn,fileDateColumn);
+            filesTable.getSortOrder().add(fileTypeColumn);
+
+            diskBox.getItems().clear();
+            for (Path p : FileSystems.getDefault().getRootDirectories()){
+                diskBox.getItems().add(p.toString());
+            }
+            diskBox.getSelectionModel().select(0);
+
+            filesTable.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
+                @Override
+                public void handle(javafx.scene.input.MouseEvent event) {
+                    if(event.getClickCount() == 2){
+                        Path path = Paths.get(pathField.getText()).resolve(filesTable.getSelectionModel().getSelectedItem().getFileName());
+                        if (Files.isDirectory(path)){
+                            updateList(path);
+                        }
+                    }
+                }
+            });
+
+            updateList((Path) Paths.get("."));
+        }
+
+        public void updateList (Path path){
+            try {
+                pathField.setText(path.normalize().toAbsolutePath().toString());
+                filesTable.getItems().clear();
+                filesTable.getItems().addAll(Files.list(path).map(FileInfo::new).collect(Collectors.toList()));
+                filesTable.sort();
+            } catch (IOException e){
+                Alert alert = new Alert(Alert.AlertType.WARNING, "Not update File List",ButtonType.OK);
+                alert.showAndWait();
+            }
+        }
+
+        public void btnPathUpAction(ActionEvent actionEvent){
+            Path upperPath = Paths.get(pathField.getText()).getParent();
+            if(upperPath != null){
+                updateList(upperPath);
+            }
+        }
+
+        public String getSelectedFileName(){
+            if (!filesTable.isFocused()){
+                return null;
+            }
+            return filesTable.getSelectionModel().getSelectedItem().getFileName();
+        }
+
+        public String getCurrentPath(){
+            return pathField.getText();
+        }
+
+        public void selectDiskAction(ActionEvent actionEvent) {
+                ComboBox<String> element = (ComboBox<String>) actionEvent.getSource();
+                updateList(Paths.get(element.getSelectionModel().getSelectedItem()));
+        }
+}
